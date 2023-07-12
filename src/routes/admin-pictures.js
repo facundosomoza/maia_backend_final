a265f92f0411a15ce26815bdae74eca03f57859d
Facundo Somoza <fsomoza@gmail.com>
2023-07-12 11:59:19 +0100

    data updated

--- src/routes/admin-pictures.js
+++ src/routes/admin-pictures.js
@@ -1,41 +1,60 @@
 const express = require("express");
 const connection = require("../connection");
+const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
 
-// Ruta para obtener los detalles de la imagen de perfil
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
+router.post("/", (req, res) => {
+  console.log("Llegue...");
 
-  // Obtiene los detalles de la imagen en la tabla admin_pictures
-  const query = "SELECT * FROM admin_pictures WHERE id = ?";
-  connection.query(query, [id], (error, results) => {
+  //Mover la imagen a la carpeta public/profile
+  const backgroundPicture = req.files.image;
+
+  const ext = path.extname(backgroundPicture.name);
+  const fileName = Date.now() + ext;
+
+  backgroundPicture.mv(`./public/images/profile/${fileName}`);
+
+  //Desactivo la imagen actual
+  const sqlResetActive = `UPDATE admin_pictures
+  SET active = 0
+  WHERE active = 1`;
+
+  connection.query(sqlResetActive, (error, result) => {
     if (error) {
-      console.error("Error al obtener los detalles de la imagen:", error);
-      res.sendStatus(500);
+      res.json({ message: "Error al cambiar la imagen" });
     } else {
-      if (results.length > 0) {
-        const imageDetails = results[0];
-        res.status(200).json(imageDetails);
-      } else {
-        res.sendStatus(404); // Imagen no encontrada
-      }
+      //Guardar en la base de datos el nombre con el que se subio la imagen
+
+      const sqlInsertProfileImage = `INSERT INTO admin_pictures(image)
+                                     VALUES( '${fileName}' )`;
+
+      connection.query(sqlInsertProfileImage, (error, result) => {
+        if (error) {
+          res.json({ message: "Error al cambiar la imagen" });
+        } else {
+          res.json({ message: "Imagen cambiada correctamente" });
+        }
+      });
     }
   });
 });
-// Ruta para actualizar la imagen de perfil
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const { image } = req.body;
 
-  // Actualiza la imagen en la tabla admin_pictures
-  const query = "UPDATE admin_pictures SET image = ? WHERE id = ?";
-  connection.query(query, [image, id], (error, results) => {
+router.get("/active-background", (req, res) => {
+  const sqlActiveBackground = `SELECT image
+                               FROM admin_pictures
+                               WHERE active = 1`;
+
+  connection.query(sqlActiveBackground, (error, result) => {
     if (error) {
-      console.error("Error al actualizar la imagen:", error);
-      res.sendStatus(500);
+      res.json({ message: "Error al obtener la imagen de fondo" });
     } else {
-      res.sendStatus(200);
+      if (result.length > 0) {
+        res.json(result[0]);
+      } else {
+        res.json(null);
+      }
     }
   });
 });
